ee761c13fbd77befd71dc2742a3374da194b9900
najoslin <nick@healtheonion.org>
2015-09-07 11:28:37 -0500

    About Page
    Nick's first cut on a simple About Page for Heroku App

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,13 +2,46 @@
 
 @section('content')
 
-<h1>ABOUT US</h1>
 <div class="container">
   <div class="row">
-    <div class="col-xs-12">
+    <div class="col-xs-6 col-md-4">
+	<img style="float:left; padding: 10px; ; margin-right: 15px; border: 3px solid green; margin-left:10px" src="../images/SuWEKC.png" Alt="Forming the team at Startup Weekend KC" height="320" width="420">
+		<div class="col-xs-12 col-md-8" style="font-family:'Open Sans'">
+		<p><h2 style="float:center"> ABOUT HEAL THE ONION LLC </h2>
+		<p style="font-size:16px">
+		We continue to work on our mission of helping people in pain find natural relief - the first time!
+		<br>
+		<br>
+		After spending over tons of money and time trying to get relief from conditions associated with multiple car wrecks, 
+		Amanda Bicknell decided there needed to be an easier way to find natural relief. &nbsp  
+		She had tried many different types of care and many different sources on the web to find care, but she saw two key problems in her search 
+		- 1) No site allowed people just to search by their pain type (they all required you already to know what kind of provider you wanted) and 
+		2) Existing review sites were unreliable because people could pay to get rated better or could flood the site with "fake" reviews. &nbsp 
+		So she set out to make the solution she wanted.  <br> <br> She took her idea to Startup Weekend KC and not only was she able to attract people who
+		were interested in the problem, the idea even attracted the attention of a couple of the mentors that decided to join the team.  &nbsp 
+		At the end of Startup Weekend Heal the Onion placed second out of 10 teams! &nbspAfter Startup 
+		Weekend the team fluctuated a bit with new members signing on and some of the original crew departing, but after a month or so - Amanda, Nick and Chris 
+		continued to develop the idea into a full fledged startup.<br><br>
+		</p>
+		</p>
+		</div>
+
+		<h2 style="float:center; width:100%; border: 1px solid green"> Our Mission</h2>
+
+		<p style="font-family:'Open Sans';font-size:16px; margin:10px">
+		Our mission is to make alternative care easier to understand and easier to access, by helping you find your relief - the first time. <br> <br>
+		This means we strive to clearly identify what providers are best for what conditions – to ensure that people in pain find the providers best 
+		able to help them and that providers receive patients that they are best suited to help. &nbsp We make the whole process simple, trustworthy 
+		and reliable – just like it would be if you were finding a natural healer through word of mouth from a trusted friend.&nbsp  
+		You are an important part of accomplishing this mission.  &nbspIf you are a member you know what works best for you and by sharing your 
+		experience, you are helping literally thousands of other people find the best alternative care for their pain.  &nbspIf you are a provider, 
+		you know who you are best suited to help and can share additional information on your modalities to help people feel more comfortable coming 
+		to you for care.<br>
+		We'll accomplish our mission by helping you find your relief - the first time!</p>
 
     </div>
   </div>
 </div>
+<br>
 
 @stop
