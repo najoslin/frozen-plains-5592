a6c915891e40e6be1d1e20d1f44e8976ed76cea6
najoslin <nick@healtheonion.org>
2015-09-07 21:36:47 -0500

    Another try
    OK one more try at the about page after looking at bootstrap some
    more....

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,46 +2,95 @@
 
 @section('content')
 
-<div class="container">
-  <div class="row">
-    <div class="" style="display:inline">
-	<img style="float:left; padding: 10px; ; margin-right: 15px; border: 3px solid green; margin-left:10px" src="../images/SuWEKC.png" Alt="Forming the team at Startup Weekend KC" height="320" width="420">
-		<div class="" style="font-family:'Open Sans'">
-		<p><h2 style="float:center"> ABOUT HEAL THE ONION LLC </h2>
-		<p style="font-size:16px">
-		We continue to work on our mission of helping people in pain find natural relief - the first time!
-		<br>
-		<br>
-		After spending over tons of money and time trying to get relief from conditions associated with multiple car wrecks, 
-		Amanda Bicknell decided there needed to be an easier way to find natural relief. &nbsp  
-		She had tried many different types of care and many different sources on the web to find care, but she saw two key problems in her search 
-		- 1) No site allowed people just to search by their pain type (they all required you already to know what kind of provider you wanted) and 
-		2) Existing review sites were unreliable because people could pay to get rated better or could flood the site with "fake" reviews. &nbsp 
-		So she set out to make the solution she wanted.  <br> <br> She took her idea to Startup Weekend KC and not only was she able to attract people who
-		were interested in the problem, the idea even attracted the attention of a couple of the mentors that decided to join the team.  &nbsp 
-		At the end of Startup Weekend Heal the Onion placed second out of 10 teams! &nbspAfter Startup 
-		Weekend the team fluctuated a bit with new members signing on and some of the original crew departing, but after a month or so - Amanda, Nick and Chris 
-		continued to develop the idea into a full fledged startup.<br><br>
-		</p>
-		</p>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<img  src="images/SuWEKC.png" Alt="Forming the team at Startup Weekend KC" height="320" width="420"/>
 		</div>
-
-		<h2 style="float:center; width:100%; border: 1px solid green"> Our Mission</h2>
-
-		<p style="font-family:'Open Sans';font-size:16px; margin:10px">
-		Our mission is to make alternative care easier to understand and easier to access, by helping you find your relief - the first time. <br> <br>
-		This means we strive to clearly identify what providers are best for what conditions – to ensure that people in pain find the providers best 
-		able to help them and that providers receive patients that they are best suited to help. &nbsp We make the whole process simple, trustworthy 
-		and reliable – just like it would be if you were finding a natural healer through word of mouth from a trusted friend.&nbsp  
-		You are an important part of accomplishing this mission.  &nbspIf you are a member you know what works best for you and by sharing your 
-		experience, you are helping literally thousands of other people find the best alternative care for their pain.  &nbspIf you are a provider, 
-		you know who you are best suited to help and can share additional information on your modalities to help people feel more comfortable coming 
-		to you for care.<br>
-		We'll accomplish our mission by helping you find your relief - the first time!</p>
-
-    </div>
-  </div>
+		<div class="col-md-8">
+			<h2 class="text-center"> ABOUT HEAL THE ONION LLC </h2>
+			<h3 class="text-center">
+				We continue to work on our mission of helping people in pain find natural relief - the first time!
+			</h3>
+			<p>
+						<br>
+						<br>
+						After spending over tons of money and time trying to get relief from conditions associated with multiple car wrecks, 
+						Amanda Bicknell decided there needed to be an easier way to find natural relief. &nbsp  
+						She had tried many different types of care and many different sources on the web to find care, but she saw two key problems in her search 
+						- 1) No site allowed people just to search by their pain type (they all required you already to know what kind of provider you wanted) and 
+						2) Existing review sites were unreliable because people could pay to get rated better or could flood the site with "fake" reviews. &nbsp 
+						So she set out to make the solution she wanted.  <br> <br> She took her idea to Startup Weekend KC and not only was she able to attract people who
+						were interested in the problem, the idea even attracted the attention of a couple of the mentors that decided to join the team.  &nbsp 
+						At the end of Startup Weekend Heal the Onion placed second out of 10 teams! &nbspAfter Startup 
+						Weekend the team fluctuated a bit with new members signing on and some of the original crew departing, but after a month or so - Amanda, Nick and Chris 
+						continued to develop the idea into a full fledged startup.<br><br>
+			</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+		<h2 class="text-center; width:100%; border: 1px solid green"> Our Mission</h2>
+			<p style="font-family:'Open Sans';font-size:16px; margin:10px">
+					Our mission is to make alternative care easier to understand and easier to access, by helping you find your relief - the first time. <br> <br>
+					This means we strive to clearly identify what providers are best for what conditions – to ensure that people in pain find the providers best 
+					able to help them and that providers receive patients that they are best suited to help. &nbsp We make the whole process simple, trustworthy 
+					and reliable – just like it would be if you were finding a natural healer through word of mouth from a trusted friend.&nbsp  
+					You are an important part of accomplishing this mission.  &nbspIf you are a member you know what works best for you and by sharing your 
+					experience, you are helping literally thousands of other people find the best alternative care for their pain.  &nbspIf you are a provider, 
+					you know who you are best suited to help and can share additional information on your modalities to help people feel more comfortable coming 
+					to you for care.<br>
+					We'll accomplish our mission by helping you find your relief - the first time!
+			</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<img src= "../images/Amanda.jpg" height="128" width="128" alt="Amanda Bicknell" style="border: 1px solid gray"> </a>
+			<h3>
+				Amanda's Story
+			</h3>
+			<p>
+				Amanda's Story goes here
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+		<div class="col-md-4">
+			<img src="../images/me.jpg" height="128" width="128" alt="Nick Joslin" style="border: 1px solid gray"></a>
+			<h3>
+				Nick's Story
+			</h3>
+			<p>
+				I was drawn to this startup because I have experience on both sides of the Alternative healing equation. &nbsp 
+				On one side I have a ruptured disk in my back that was operated on years ago.  &nbsp I continue to work and live, but I 
+				experience chronic lower back pain relating to this injury and have sought out different kinds of pain relief from 
+				massage to acupuncture.  &nbsp I know how hard it can be to find the right provider to help you get relief. <br> <br>
+				On the other side of the equation, my wife is a licensed massage therapist, a yoga instructor and a herablist and I 
+				know how hard it has been for her in starting her business and attracting clients that she is best suited to help. &nbsp
+				She is active in the natural healing community and we frequently talk about how to better reach people who could
+				benefit the most from her skills.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+		<div class="col-md-4">
+			<img src="../images/Chris.jpg" height="128" width="128" alt="Chris Morris" style="border: 1px solid gray"></a>
+			<h3>
+				Chris's Story
+			</h3>
+			<p>
+				Chris's Story goes here
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+	</div>
 </div>
+
 <br>
 
 @stop
